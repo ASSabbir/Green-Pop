97f5df688a57a8bc6e13b16d2d19e628f9d03fb3
ASSabbir <atiksabbir1125@gmail.com>
2024-05-19 22:32:09 +0600

    added resposve

--- src/Componetns/Products/Products.jsx
+++ src/Componetns/Products/Products.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Top from '../Home/Cards/Top';
 // https://i.ibb.co/gmnX7rW/josefin-WS5yj-Fjyc-NY-unsplash.jpg
 const Products = () => {
+    const [flag, setFlag] = useState(1)
     const data = useLoaderData()
-    console.log(data)
-    const demo = data[0]
+
+    console.log(flag)
     return (
         <div>
             <div>
-                <div className="hero " style={{ backgroundImage: 'url(https://i.ibb.co/gmnX7rW/josefin-WS5yj-Fjyc-NY-unsplash.jpg)' }}>
+                <div className="hero w-fit md:w-full " style={{ backgroundImage: 'url(https://i.ibb.co/gmnX7rW/josefin-WS5yj-Fjyc-NY-unsplash.jpg)' }}>
                     <div className="hero-overlay bg-opacity-60 "></div>
                     <div className="hero-content text-center text-neutral-content">
                         <div className="p-10">
@@ -18,26 +19,26 @@ const Products = () => {
                         </div>
                     </div>
                 </div>
-                <section className='flex max-w-screen-2xl mx-auto mt-40'>
+                <section className='flex md:flex-row flex-col max-w-screen-2xl mx-auto md:mt-40 mt-10'>
                     {/* sectiion 1  */}
-                    <section className='w-[30%] space-y-10'>
+                    <section className='md:w-[30%] w-full mb-20   px-5 md:px-0  space-y-10'>
                         <div>
-                            <h1 className='text-3xl font-bold'>Categories</h1>
-                            <select className="select select-ghost w-full max-w-xs">
-                                <option disabled selected>Pick the best JS framework</option>
-                                <option>Svelte</option>
-                                <option>Vue</option>
-                                <option>React</option>
+                            <h1 className='text-3xl font-bold pb-5'>Categories</h1>
+                            <select className="select select-ghost focus:outline-none focus:border-0 outline-none w-full max-w-xs">
+                                <option disabled selected>All Categories</option>
+                                <option>In Door</option>
+                                <option>Out Door</option>
+
                             </select>
                         </div>
                         <div>
-                            <h1 className='text-3xl font-bold'>Price</h1>
+                            <h1 className='text-3xl font-bold pb-5'>Price</h1>
                             <input type="range" min={0} max="100" className="range max-w-xs range-xs" />
 
                         </div>
 
                         <div>
-                            <h1 className='text-3xl font-bold'>Size</h1>
+                            <h1 className='text-3xl font-bold pb-5'>Size</h1>
                             <div>
                                 <div className='space-x-5'>
                                     <h1 className='border-2 inline-block p-2 px-4 focus:border-black hover:border-black duration-300'>L</h1>
@@ -48,16 +49,16 @@ const Products = () => {
                             </div>
                         </div>
                         <div>
-                            <h1 className='text-3xl font-bold'>Feature Product</h1>
+                            <h1 className='text-3xl font-bold pb-5'>Feature Product</h1>
                             <div>
                                 {
                                     data.slice(8, 10).map(data => <>
-                                        <div className="w-36 gap-5 rounded-md flex   text-gray-900">
+                                        <div className="w-36 gap-5 pb-3 rounded-md flex   text-gray-900">
                                             <img src={data.image} alt="" className="object-cover object-center w-full rounded-md h-44  bg-gray-500" />
                                             <div className="mt-6 mb-2 space-y-2">
                                                 <div className="rating">
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
+                                                    <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
                                                     <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
@@ -73,11 +74,28 @@ const Products = () => {
                         </div>
 
                     </section>
-                    <section className='w-[70%]'>
-                        <div className='grid grid-cols-3 gap-5'>
-                            {
-                                data.map(data => <Top data={data}></Top>)
+
+                    {/* section 2  */}
+                    <section className='md:w-[70%]'>
+                        <h1 className='text-6xl font-bold md:hidden flex font-reemKufi text-center text-gray-600  pb-20'><span className='text-primary'>P</span>roducts</h1>
+                        <div className='grid grid-cols-1 px-2 md:grid-cols-3 gap-5'>
+                            {flag == 1 &&
+                                data.slice(0, 6).map(data => <Top data={data}></Top>) ||
+                                flag == 2 &&
+                                data.slice(6, 12).map(data => <Top data={data}></Top>) ||
+                                flag == 3 &&
+                                data.slice(12, 18).map(data => <Top data={data}></Top>) ||
+                                flag == 4 &&
+                                data.slice(18, 20).map(data => <Top data={data}></Top>)
                             }
+                        </div>
+                        <div className='w-full flex items-center justify-center '>
+                            <div className="join ">
+                                <input onClick={() => setFlag(1)} className={`join-item btn  ${flag == 1 && 'bg-[#3F00E7] text-white'}`} type="radio" name="options" aria-label="1" />
+                                <input onClick={() => setFlag(2)} className="join-item btn btn-square" type="radio" name="options" aria-label="2" />
+                                <input onClick={() => setFlag(3)} className="join-item btn " type="radio" name="options" aria-label="3" />
+                                <input onClick={() => setFlag(4)} className="join-item btn btn-square" type="radio" name="options" aria-label="4" />
+                            </div>
                         </div>
                     </section>
                 </section>
